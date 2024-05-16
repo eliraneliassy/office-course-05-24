36dadebf7595611b385bfa6f96cef61f6d5a22c1
Eliran Eliassy <eliran@e-square.io>
2024-05-16 15:52:49 +0300

    cva

--- apps/ui/src/lib/input.component.ts
+++ apps/ui/src/lib/input.component.ts
@@ -1,6 +1,13 @@
-import { ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, inject, Input, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  FormsModule, NG_VALIDATORS,
+  NG_VALUE_ACCESSOR, NgControl,
+  ValidationErrors,
+  Validator, ValidatorFn, Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'lib-input',
@@ -10,14 +17,25 @@ import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/f
   styleUrl: './input.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      multi: true,
-      useExisting: InputComponent
-    }
+    // {
+    //   provide: NG_VALUE_ACCESSOR,
+    //   multi: true,
+    //   useExisting: InputComponent
+    // },
+    // {
+    //   provide: NG_VALIDATORS,
+    //   multi: true,
+    //   useExisting: InputComponent
+    // }
   ]
 })
-export class InputComponent implements ControlValueAccessor{
+export class InputComponent implements ControlValueAccessor, Validator {
+
+  controlDir: NgControl = inject(NgControl, {self: true})
+  constructor() {
+    this.controlDir.valueAccessor = this;
+  }
+
 
   @Input() label = '';
   @Input() placeholder = '';
@@ -50,4 +68,17 @@ export class InputComponent implements ControlValueAccessor{
   setDisabledState?(isDisabled: boolean): void {
     this.disabled = isDisabled;
   }
+
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    const validators: ValidatorFn[] = [];
+    if(this.required) {
+      validators.push(Validators.required)
+    }
+
+    return validators;
+
+  }
+  // registerOnValidatorChange?(fn: () => void): void {
+  //   throw new Error('Method not implemented.');
+  // }
 }
